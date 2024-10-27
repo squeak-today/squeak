bb97d405803cfe290f70d65e4fa46ae391498245
connortbot <loiconnor8@gmail.com>
2024-10-27 18:45:41 -0400

    separate out provider tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,10 +9,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-east-2"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-050cd642fd83388e4" # amazon linux 2023 AMI
   instance_type = "t2.micro"
