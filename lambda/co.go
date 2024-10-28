cd831c4f61c1dab9a9e0034c58981ce88138d925
connortbot <loiconnor8@gmail.com>
2024-10-28 14:51:01 -0400

    cohere req with custom prompt

---
+++ lambda/co.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"io"
+	"errors"
+	"bytes"
+)
+
+type ChatTurn struct {
+    Text string `json:"text"`
+}
+
+type Message struct {
+    Content []ChatTurn `json:"content"`
+}
+
+type Response struct {
+    Message Message `json:"message"`
+}
+
+func generateStory(language string, cefr string, topic string) (string, error) {
+	cohereAPIKey := os.Getenv("COHERE_API_KEY")
+	if cohereAPIKey == "" { return "", errors.New("ERR: COHERE_API_KEY environment variable not set") }
+	fmt.Println("LOG: Running with Cohere API KEY ->", cohereAPIKey)
+
+	startingMessage := fmt.Sprintf("LANGUAGE: %s\nCEFR: %s\nTOPIC: %s", language, cefr, topic)
+	coherePayload := map[string]interface{}{
+        "model": "c4ai-aya-23-35b",
+        "messages": []map[string]string{
+            {
+                "role": "system",
+                "content": 
+					`You are Squeak, an LLM designed to write short stories or news stories. 
+					The nature of the story is dependent on CEFR, LANGUAGE, TOPIC, and USEFUL WORDS.
+					You must write your story in the LANGUAGE on TOPIC using at least one of the USEFUL WORDS translated without sacrificing story quality.
+					The difficulty of the story MUST be understandable to the CEFR provided.
+					You should provide the story without preamble or other comment.`,
+            },
+			{
+				"role": "user",
+				"content": startingMessage,
+			},
+        },
+    }
+
+	jsonData, err := json.Marshal(coherePayload)
+    if err != nil { return "", err }
+
+	req, err := http.NewRequest("POST", "https://api.cohere.com/v2/chat", bytes.NewBuffer(jsonData))
+    if err != nil { return "", err }
+
+	req.Header.Set("Accept", "application/json")
+    req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("Authorization", "Bearer " + cohereAPIKey)
+
+	client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil { return "", err }
+    defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+    if err != nil { return "", err }
+
+    // var result map[string]interface{}
+    var result Response
+    if err := json.Unmarshal(body, &result); err != nil { return "", err }
+
+	story := result.Message.Content[0].Text
+	return story, nil
+}
--- lambda/main.go
+++ lambda/main.go
@@ -2,91 +2,17 @@ package main
 
 // https://github.com/cohere-ai/cohere-go
 // https://docs.cohere.com/v2/docs/cohere-works-everywhere#cohere-platform
-// COHERE_API_KEY="api-key" go run main.go
+// COHERE_API_KEY="api-key" go run .
 
 import (
-	"bytes"
-    "encoding/json"
     "fmt"
-    "net/http"
-    "os"
-	"log"
-	"io"
 )
 
-type ChatTurn struct {
-    Text string `json:"text"`
-}
-
-type Message struct {
-    Content []ChatTurn `json:"content"`
-}
-
-type Response struct {
-    Message Message `json:"message"`
-}
-
 func main() {
-	cohereAPIKey := os.Getenv("COHERE_API_KEY")
-	if cohereAPIKey == "" {
-		log.Fatal("COHERE_API_KEY environment variable not set")
-	}
-
-	fmt.Println("RUNNING WITH API KEY:", cohereAPIKey)
-
-	coherePayload := map[string]interface{}{
-        "model": "command-r-plus-08-2024",
-        "messages": []map[string]string{
-            {
-                "role":    "user",
-                "content": "Hello world!",
-            },
-        },
+    story, err := generateStory("French", "B2", "Stephen Curry")
+    if err == nil {
+        fmt.Println("Story:", story)
+    } else {
+        fmt.Println(err)
     }
-
-	jsonData, err := json.Marshal(coherePayload)
-    if err != nil {
-        fmt.Println("Error marshalling JSON:", err)
-        return
-    }
-
-	req, err := http.NewRequest("POST", "https://api.cohere.com/v2/chat", bytes.NewBuffer(jsonData))
-    if err != nil {
-        fmt.Println("Error creating request:", err)
-        return
-    }
-
-	req.Header.Set("Accept", "application/json")
-    req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("Authorization", "Bearer " + cohereAPIKey)
-
-	client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        fmt.Println("Error making POST request:", err)
-        return
-    }
-    defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Println("Error reading response body:", err)
-        return
-    }
-
-    // var result map[string]interface{}
-    var result Response
-    if err := json.Unmarshal(body, &result); err != nil {
-        fmt.Println("Error unmarshalling JSON:", err)
-        return
-    }
-
-    for i, item := range result.Message.Content {
-        fmt.Printf("Text %d: %s\n", i, item.Text)
-    }
-    
-//     var message = result["message"]
-//     content := message.(map[string]interface{})["content"]
-//     text := content.([]interface{})[0].(map[string]interface{})["text"]
-//     fmt.Println("Response:", text.(string))
 }
