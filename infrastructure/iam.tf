f008b0d1f1dced71297d4fa5c86184ed5a8db5f3
connortbot <loiconnor8@gmail.com>
2024-10-28 22:02:43 -0400

    new role for front lambda

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -5,8 +5,61 @@ locals {
     account_id = data.aws_caller_identity.current.account_id
 }
 
+# Story Generation IAM (backend lambda)
 resource "aws_iam_role" "story_gen_role" {
     name = "story-gen-role"
+
+    assume_role_policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow"
+            }
+        ]
+    })
+
+    inline_policy {
+        name = "lambda-policies"
+        policy = jsonencode({
+            "Version" : "2012-10-17",
+            "Statement" : [
+                # Cloudwatch Logs permissions
+                {
+                    "Effect" : "Allow",
+                    "Action" : "logs:CreateLogGroup",
+                    "Resource" : "arn:aws:logs:${data.aws_region.current.name}:${local.account_id}:*"
+                },
+                {
+                    "Effect" : "Allow",
+                    "Action" : [
+                        "logs:CreateLogStream",
+                        "logs:PutLogEvents"
+                    ],
+                    "Resource" : [
+                        "arn:aws:logs:${data.aws_region.current.name}:${local.account_id}:log-group:/aws/lambda/*:*"
+                    ]
+                },
+                # S3 Permissions for storing stories
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "s3:PutObject",
+                        "s3:PutobjectAcl"
+                    ],
+                    "Resource": "arn:aws:s3:::${var.s3_bucket_name}/*"
+                }
+            ]
+        })
+    }
+}
+
+# Story API IAM (frontend lambda)
+resource "aws_iam_role" "story_api_role" {
+    name = "story-api-role"
 
     assume_role_policy = jsonencode({
         "Version": "2012-10-17",
@@ -42,14 +95,17 @@ resource "aws_iam_role" "story_gen_role" {
                         "arn:aws:logs:${data.aws_region.current.name}:${local.account_id}:log-group:/aws/lambda/*:*"
                     ]
                 },
-                # S3 Permissions for storing stories
+                # S3 Permissions for getting the stories
                 {
                     "Effect": "Allow",
                     "Action": [
-                        "s3:PutObject",
-                        "s3:PutobjectAcl"
+                        "s3:GetObject",
+                        "s3:ListBucket"
                     ],
-                    "Resource": "arn:aws:s3:::${var.s3_bucket_name}/*"
+                    "Resource": [
+                        "arn:aws:s3:::${var.s3_bucket_name}",
+                        "arn:aws:s3:::${var.s3_bucket_name}/*"
+                    ]
                 }
             ]
         })
