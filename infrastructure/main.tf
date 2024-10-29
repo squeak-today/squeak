528ee97b7703acd5b07f7d717f3b094e4dd11868
connortbot <loiconnor8@gmail.com>
2024-10-28 21:53:26 -0400

    no need for ec2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,12 +8,3 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-050cd642fd83388e4" # amazon linux 2023 AMI
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ec2-squeak"
-  }
-}
