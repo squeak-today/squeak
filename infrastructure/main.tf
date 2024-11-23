c3c6c73ec32c6903a315c87060c01a72bed8f837
connortbot <loiconnor8@gmail.com>
2024-11-22 23:57:33 -0500

    tf app client with outputs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,4 +19,32 @@ resource "aws_cognito_user_pool" "user_pool" {
 	admin_create_user_config {
     	allow_admin_create_user_only = false
 	}
+
+	verification_message_template {
+		default_email_option = "CONFIRM_WITH_CODE"
+		email_message = "Your verification code is {####}"
+		email_subject = "Verify your email"
+	}
+
+}
+
+resource "aws_cognito_user_pool_client" "app_client" {
+	name = "squeak_app_client"
+	user_pool_id = aws_cognito_user_pool.user_pool.id
+
+	explicit_auth_flows = [
+		"ALLOW_USER_PASSWORD_AUTH",
+		"ALLOW_CUSTOM_AUTH", //unused for now
+		"ALLOW_USER_SRP_AUTH" //unused for now
+	]
+
+	generate_secret = false
+}
+
+output "user_pool_id" {
+	value = aws_cognito_user_pool.user_pool.id
+}
+
+output "app_client_id" {
+	value = aws_cognito_user_pool_client.app_client.id
 }
