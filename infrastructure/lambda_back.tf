074c1a13a2ab7b5a541812904e10d83ec6d5cf9d
connortbot <loiconnor8@gmail.com>
2024-11-30 22:11:09 -0500

    story gen sqs queue

--- infrastructure/lambda_back.tf
+++ infrastructure/lambda_back.tf
@@ -7,6 +7,15 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
     name = "/aws/lambda/story-gen-lambda"
 }
 
+# no dead letter queue assigned?
+resource "aws_sqs_queue" "story_gen_queue" {
+	name = "story-gen-queue"
+	delay_seconds = 0
+	max_message_size = 262144  # 256kb
+	message_retention_seconds = 345600  # 4 days
+	receive_wait_time_seconds = 10
+}
+
 // standard
 resource "aws_lambda_function" "story_gen_lambda" {
     function_name = "story-gen-lambda"
@@ -27,6 +36,7 @@ resource "aws_lambda_function" "story_gen_lambda" {
             COHERE_API_KEY = var.cohere_api_key
 			TAVILY_API_KEY = var.tavily_api_key
             STORY_BUCKET_NAME = aws_s3_bucket.story_gen_bucket.bucket
+			SQS_QUEUE_URL = aws_sqs_queue.story_gen_queue.name
         }
     }
 
