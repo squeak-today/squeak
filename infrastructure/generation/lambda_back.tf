ac20953caa1f3707802f7f0b2332a7263961265c
connortbot <loiconnor8@gmail.com>
2024-12-02 12:05:57 -0500

    scale lambda invocations via sqs queue

--- infrastructure/generation/lambda_back.tf
+++ infrastructure/generation/lambda_back.tf
@@ -34,6 +34,7 @@ resource "aws_lambda_function" "story_gen_lambda" {
 
     depends_on = [
         aws_iam_role.story_gen_role,
+		aws_sqs_queue.story_gen_queue,
         var.story_gen_bucket_arn, # ensure story gen bucket is made
         aws_cloudwatch_log_group.lambda_log_group
     ]
@@ -43,10 +44,10 @@ resource "aws_lambda_function" "story_gen_lambda" {
     }
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_split_lambda" {
-    statement_id  = "AllowExecutionFromCloudWatch"
-    action        = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.story_gen_lambda.function_name
-    principal     = "events.amazonaws.com"
-    source_arn    = aws_cloudwatch_event_rule.lambda_trigger.arn
-}
+# resource "aws_lambda_permission" "allow_cloudwatch_to_call_split_lambda" {
+#     statement_id  = "AllowExecutionFromCloudWatch"
+#     action        = "lambda:InvokeFunction"
+#     function_name = aws_lambda_function.story_gen_lambda.function_name
+#     principal     = "events.amazonaws.com"
+#     source_arn    = aws_cloudwatch_event_rule.lambda_trigger.arn
+# }
--- infrastructure/generation/main.tf
+++ infrastructure/generation/main.tf
@@ -6,15 +6,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-
-# SQS queue
-resource "aws_sqs_queue" "story_gen_queue" {
-	name = "story-gen-queue"
-	delay_seconds = 0
-	max_message_size = 262144  # 256kb
-	message_retention_seconds = 345600  # 4 days
-	receive_wait_time_seconds = 10
-
-	# no dead letter queue assigned?
-}
