61806485377cd3c11065b46b3270919d2d88bf99
connortbot <loiconnor8@gmail.com>
2025-01-03 13:57:56 -0500

    add missing dependencies for options integration response

--- infrastructure/api_gateway/main.tf
+++ infrastructure/api_gateway/main.tf
@@ -84,4 +84,9 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
         "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
     }
+
+    depends_on = [
+        aws_api_gateway_integration.options_integration,
+        aws_api_gateway_method_response.options_method_response
+    ]
 }
