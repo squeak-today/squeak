5c17cf4911bdf79545a194cada9761cda79862c9
connortbot <loiconnor8@gmail.com>
2025-01-12 23:14:16 -0500

    news and stories query endpoint

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -32,7 +32,15 @@ module "news_query" {
 	source            = "./api_gateway"
 	rest_api_id       = aws_api_gateway_rest_api.story_api.id
 	parent_id         = aws_api_gateway_rest_api.story_api.root_resource_id
-	path_part         = "query"
+	path_part         = "news-query"
+	lambda_arn        = aws_lambda_function.story_api_lambda.invoke_arn
+}
+
+module "story_query" {
+	source            = "./api_gateway"
+	rest_api_id       = aws_api_gateway_rest_api.story_api.id
+	parent_id         = aws_api_gateway_rest_api.story_api.root_resource_id
+	path_part         = "story-query"
 	lambda_arn        = aws_lambda_function.story_api_lambda.invoke_arn
 }
 
