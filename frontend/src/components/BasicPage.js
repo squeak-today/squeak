25b0023cba887ebcbabfacab0a0a631ec88bcfa6
Pranav Bedi <pranav.bedi.work@gmail.com>
2025-01-25 20:02:14 -0500

    changed the top bar button to link to new form

--- frontend/src/components/BasicPage.js
+++ frontend/src/components/BasicPage.js
@@ -28,10 +28,7 @@ function BasicPage({ children, showLogout, onLogout }) {
           
           <ButtonContainer>
             <MiscButton
-              as="a"
-              href="https://forms.gle/LumHWSYaqLKV4KMa8"
-              target="_blank"
-              rel="noopener noreferrer"
+              as="a" href="/contact-support.html" target="_blank" rel="noopener noreferrer"
             >
               Tell Us Anything! ❤️
             </MiscButton>
