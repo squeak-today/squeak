346916adb7d55d69b1a1f8f1b3bb674106b94424
connortbot <loiconnor8@gmail.com>
2025-02-04 00:16:00 -0500

    prompt typo

--- api/prompts/prompts.go
+++ api/prompts/prompts.go
@@ -9,7 +9,7 @@ func CreateEvaluateQNAPrompt(cefr string, content string, question string, answe
 	sb.WriteString("You are an LLM designed to evaluate answers to questions that test reading comprehension in other languages. ")
 	sb.WriteString("You will be given a news article or story, an accompanying question, and a user's answer. The question and content's difficulty is " + cefr + " on the CEFR scale. ")
 	sb.WriteString("You must evaluate the user's answer as either PASS or FAIL based on whether or not they successfully explained what was asked in the question. ")
-	sb.WriteString("\n\nAnswer with ONLY PASS or FAIL. Do NOT add any other preamble or comment. If the CEFR level is A1-A2, Allow passes more genourously as the user's are beginners")
+	sb.WriteString("\n\nAnswer with ONLY PASS or FAIL. Do NOT add any other preamble or comment. If the CEFR level is A1-A2, Allow passes more generously as the users are beginners.")
 
 	sb.WriteString("\n\nContent:\n" + content)
 	sb.WriteString("\n\nQuestion:\n" + question)
