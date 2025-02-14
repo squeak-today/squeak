b0ebeffe8f6020397125af286456b0f4cf71cf50
connortbot <loiconnor8@gmail.com>
2025-02-14 09:18:00 -0500

    update outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,6 +1,11 @@
-output "api_gateway_url_content" {
-  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/content"
-  description = "Content URL for Story API"
+output "api_gateway_url_story" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/story"
+  description = "Story URL for Story API"
+}
+
+output "api_gateway_url_story_context" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/story/context"
+  description = "Story Context URL for Story API"
 }
 
 output "api_gateway_url_news" {
@@ -8,29 +13,29 @@ output "api_gateway_url_news" {
   description = "News URL for Story API"
 }
 
+output "api_gateway_url_news_query" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/news/query"
+  description = "News Query URL for Story API"
+}
+
+output "api_gateway_url_story_query" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/story/query"
+  description = "Story Query URL for Story API"
+}
+
 output "api_gateway_url_translate" {
   value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/translate"
   description = "Translate URL for Story API"
 }
 
-output "api_gateway_url_evaluate_qna" {
-  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/evaluate-qna"
-  description = "Evaluate QNA URL for Story API"
+output "api_gateway_url_qna" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/qna"
+  description = "QNA URL for Story API"
 }
 
-output "api_gateway_url_news_query" {
-  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/news-query"
-  description = "News Query URL for Story API"
-}
-
-output "api_gateway_url_story_query" {
-  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/story-query"
-  description = "Story Query URL for Story API"
-}
-
-output "api_gateway_url_content_question" {
-  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/content-question"
-  description = "Content Question URL for Story API"
+output "api_gateway_url_qna_evaluate" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/qna/evaluate"
+  description = "QNA Evaluate URL for Story API"
 }
 
 output "api_gateway_url_profile" {
@@ -38,7 +43,22 @@ output "api_gateway_url_profile" {
   description = "Profile URL for Story API"
 }
 
-output "api_gateway_url_profile_update" {
-  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/profile-upsert"
-  description = "Profile Update URL for Story API"
+output "api_gateway_url_profile_upsert" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/profile/upsert"
+  description = "Profile Upsert URL for Story API"
+}
+
+output "api_gateway_url_progress" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/progress"
+  description = "Progress URL for Story API"
+}
+
+output "api_gateway_url_progress_increment" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/progress/increment"
+  description = "Progress Increment URL for Story API"
+}
+
+output "api_gateway_url_progress_streak" {
+  value       = "https://${aws_api_gateway_rest_api.story_api.id}.execute-api.us-east-2.amazonaws.com/${terraform.workspace}/progress/streak"
+  description = "Progress Streak URL for Story API"
 }
