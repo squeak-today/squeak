cca08ab2a4343baca459e081d630222cd2cb7054
Saai151 <94809929+Saai151@users.noreply.github.com>
2025-02-15 20:34:27 -0500

    adding teacher class accept module to gateway.tf

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -168,6 +168,15 @@ module "student_classroom_join" {
   lambda_arn  = aws_lambda_function.story_api_lambda.invoke_arn
 }
 
+module "teacher_class_accept"{
+  source = "./api_gateway"
+  rest_api_id = aws_api_gateway_rest_api.story_api.id
+  parent_id = module.teacher_classroom.resource_id
+  path_part = "accept"
+  http_method = "POST"
+  lambda_arn = "aws_lambda_function.story_api_lambda.invoke_arn"
+}
+
 # Lambda permissions
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "${terraform.workspace}-AllowExecutionFromAPIGateway"
