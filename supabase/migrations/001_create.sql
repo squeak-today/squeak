679ccd5d2914f9f56a48501ecd0fb34e94a310bc
connortbot <loiconnor8@gmail.com>
2025-02-17 15:53:29 -0500

    itempotent pages statements

--- supabase/migrations/001_create.sql
+++ supabase/migrations/001_create.sql
@@ -25,14 +25,8 @@ ALTER TABLE news
 DROP CONSTRAINT IF EXISTS unique_news_entry;
 
 ALTER TABLE news
-DROP CONSTRAINT IF EXISTS unique_news_entry;
-
-ALTER TABLE news
 ADD CONSTRAINT unique_news_entry 
 UNIQUE (topic, language, cefr_level, date_created);
-
-ALTER TABLE stories 
-DROP CONSTRAINT IF EXISTS unique_stories_entry;
 
 ALTER TABLE stories 
 DROP CONSTRAINT IF EXISTS unique_stories_entry;
